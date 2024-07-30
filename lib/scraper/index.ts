bd78438a3c97069eee0d2b96d3357ccd41004a72
Utkarsh Dixit <utkarshdixit.2k21@gmail.com>
2024-07-30 19:20:48 +0530

    scraped several data from amazon

--- lib/scraper/index.ts
+++ lib/scraper/index.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 import * as cheerio from "cheerio";
+import { extractPrice, extractCurrency } from "../utils";
 
 export async function scrapeAmazonProduct(url: string) {
   if (!url) {
@@ -26,8 +27,48 @@ export async function scrapeAmazonProduct(url: string) {
     const $ = cheerio.load(response.data);
 
     const title = $("#productTitle").text().trim();
+    const currentPrice = extractPrice(
+      $(".priceToPay span.a-price-whole"),
+      $("a.size.base.a-color-price"),
+      $(".a-button-selected .a-color-base")
+    );
 
-    console.log(title);
+    const originalPrice = extractPrice(
+      $("#priceblock_ourprice"),
+      $(".a-price.a-text-price span.a-offscreen"),
+      $(".a-section span.aok-relative"),
+      $("#listPrice"),
+      $("#priceblock_dealprice"),
+      $(".a-size-base.a-color-price")
+    );
+
+    const outOfStock =
+      $("#availability span.a-size-medium.a-color-price")
+        .text()
+        .trim()
+        .toLowerCase() === "currently unavailable";
+
+    const image =
+      $("#imgBlkFront").attr("data-a-dynamic-image") ||
+      $("#landingImage").attr("data-a-dynamic-image") ||
+      "{}";
+
+    const imageUrls = Object.keys(JSON.parse(image));
+
+    const currency = extractCurrency($(".a-price-symbol"));
+
+    const rating = $(".a-popover-trigger span.a-size-base.a-color-base")
+      .text()
+      .trim();
+    console.log(
+      { title },
+      { currentPrice },
+      { originalPrice },
+      { rating },
+      { outOfStock },
+      { imageUrls },
+      { currency }
+    );
   } catch (error: any) {
     throw new Error(`Failed to scrape Amazon product: ${error.message}`);
   }
